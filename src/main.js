ce2c9e53f0ffef24ae30002d8b49ca6797a8d5b0
Manfred Zingl <mz@campudus.com>
2019-07-16 15:27:59 +0200

    changed source

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<h1>Hello Jenkins Multibranch Pipeline</h1>\n<p>changed?</p>");
+  response.end("<h1>Hello Jenkins Multibranch Pipeline</h1>\n");
 });
 
 server.listen(8000);
